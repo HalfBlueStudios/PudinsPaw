5b635c6ef3502d0b53e4dd06ba9aac8a47928d8c
Johan Donkersloot <donker10@uw.edu>
2016-03-30 10:31:17 -0700

    3/30/16
    inital commit

---
+++ siteWide.js
@@ -0,0 +1,25 @@
+﻿
+var selectedBarName = ".selectedBar";
+
+var main = function()
+{
+    $('.navigationBar').find('li').each(
+        function () {
+            var newWidth = $(this).find('h1').width() * 1.2;
+            $(this).find(selectedBarName).css("width", newWidth);
+        }
+        );
+    $('.navigationBar').find('li').mouseleave(
+        function () {
+            $(this).find(selectedBarName).css("visibility", "hidden");
+        }
+        );
+    $('.navigationBar').find('li').mouseover(
+        function () {
+            $(this).find(selectedBarName).css("visibility", "visible");
+        }
+        );
+}
+
+
+$(document).ready(main);
