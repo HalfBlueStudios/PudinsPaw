e49372f5310363bcb29f9bbae2719bfb9b9268ba
HalfBlueStudios <donker10@uw.edu>
2016-04-05 17:52:18 -0700

    4/5/16
    Fixed sizes and made the navigation bar load from one file so it can be
    easily changed

--- siteWide/javascript/navBar.js
+++ siteWide/javascript/navBar.js
@@ -3,6 +3,8 @@ var selectedBarName = ".selectedBar";
 
 var main = function()
 {
+    setNavigationBar();
+    $('.navigationBar').find('.navBox').css('width', $(window).width());
     $('.navigationBar').find('li').each(
         function () {
             var newWidth = $(this).find('h1').width() * 1.2;
@@ -19,6 +21,42 @@ var main = function()
             $(this).find(selectedBarName).css("visibility", "visible");
         }
         );
+}
+
+var setNavigationBar = function()
+{
+    $('.navigationBar').html("<div class=\"navBox\" height:200px width:300px>" +
+        "<ul>" +
+            "<li>" +
+                "<div class=\"selectedWrapper\">" +
+                    "<div class=\"selectedBar\"></div>" +
+                "</div>" +
+                "<h1><a href=\"HomePage.html\">Home</a></h1>" +
+            "</li>" +
+            "<li>" +
+                "<div class=\"selectedWrapper\">" +
+                    "<div class=\"selectedBar\"></div>" +
+                "</div>" +
+                "<h1><a href=\"buildACollar.html\">Build a collar</a></h1>" +
+            "</li>" +
+            "<li>" +
+                "<div class=\"selectedWrapper\">" +
+                    "<div class=\"selectedBar\"></div>" +
+                "</div>" +
+                "<h1><a href=\"Petimonials.html\">About Us</a></h1>" +
+            "</li>" +
+        "</ul>" +
+        "<div class=\"userBar\">" +
+            "<ul>" +
+                "<li>" +
+                    "<h1>Account</h1>" +
+                "</li>" +
+                "<li>" +
+                    "<img src=\"siteWide/images/shopping_cart_icon.png\" />" +
+                "</li>" +
+            "</ul>" +
+        "</div>" +
+    "</div>");
 }
 
 
